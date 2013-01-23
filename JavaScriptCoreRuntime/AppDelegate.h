450ecc7198102ecec73809144b2c8f3172aaea56
Francis Chong <francis@ignition.hk>
2013-01-23 23:10:05 +0800

    Initial Commit

---
+++ JavaScriptCoreRuntime/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JavaScriptCoreRuntime
+//
+//  Created by Chong Francis on 13年1月23日.
+//  Copyright (c) 2013年 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JavaScriptCoreRuntime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JavaScriptCoreRuntime
+//
+//  Created by Chong Francis on 13年1月23日.
+//  Copyright (c) 2013年 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
